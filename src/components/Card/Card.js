e24c81f09d114687b80cd40d4100d35fbc15df18
Claire Fields <clairefields15@gmail.com>
2021-09-18 19:17:59 -0600

    Add show more button to photo explanation

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
 export const Card = ({ photo, handleLike, liked }) => {
+  const [showMoreText, setShowMoreText] = useState(false);
+
   const handleClick = e => {
     e.preventDefault();
     const id = e.target.closest('article').id;
     handleLike(id);
+  };
+
+  const showMore = e => {
+    e.preventDefault();
+    setShowMoreText(true);
   };
 
   return (
@@ -13,9 +20,17 @@ export const Card = ({ photo, handleLike, liked }) => {
       {photo.hdurl && <img src={photo.hdurl} alt={photo.explanation} />}
       <h3>{photo.title}</h3>
       <p>{photo.date}</p>
-      {photo.copyright && <p>Photo by: {photo.copyright}</p>}{' '}
-      <p>{photo.explanation}</p>
-      {!liked && <button onClick={e => handleClick(e)}>Like</button>}{' '}
+      {photo.copyright && <p>Photo by: {photo.copyright}</p>}
+      <p>
+        {!showMoreText && (
+          <>
+            {photo.explanation.slice(0, 120) + '...'}
+            <button onClick={e => showMore(e)}>More</button>
+          </>
+        )}
+        {showMoreText && photo.explanation}
+      </p>
+      {!liked && <button onClick={e => handleClick(e)}>Like</button>}
       {liked && <button onClick={e => handleClick(e)}>Dislike</button>}
     </article>
   );
