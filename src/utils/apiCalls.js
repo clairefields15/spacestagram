69661aa3a0d71632252e75c44e339cee8a147157
Claire Fields <clairefields15@gmail.com>
2021-09-19 17:16:56 -0600

    Add a readme and increase photo count to 15

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,6 +1,7 @@
 export const fetchPhotos = async () => {
   const key = process.env.REACT_APP_API_KEY;
-  const count = 5;
+
+  const count = 15;
   let uri = `https://api.nasa.gov/planetary/apod?api_key=${key}&count=${count}`;
   let response = await fetch(uri);
   let data = await checkForErrors(response);
@@ -11,11 +12,11 @@ const checkForErrors = response => {
   if (response.status === 200) {
     return response.json();
   } else if (response.status === 404) {
-    throw new Error('404: having trouble locating photos now');
+    throw new Error("Oops, we're having trouble loading photos now");
   } else if (response.status === 500) {
-    throw new Error('Oops our servers are down, please try again later');
+    throw new Error('Sorry, our servers are down, please try again later');
   } else {
-    throw new Error('Something went wrong');
+    throw new Error('Launch failed, try again later');
   }
 };
 
